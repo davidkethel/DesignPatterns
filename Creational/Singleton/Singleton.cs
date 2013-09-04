33e5001a5720807600bbe3e0caa9053127ef5caf
davidkethel <davidkethel@gmail.com>
2013-09-05 09:30:38 +1000

    Started single ton

---
+++ Creational/Singleton/Singleton.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Creational.Singleton
+{
+   public sealed class Singleton
+   {
+
+       private static Singleton _instance = null;
+
+       private Singleton(){}
+
+       public static Singleton Instance
+       {
+           get { return _instance ?? (_instance = new Singleton()); }
+       }
+   }
+}
