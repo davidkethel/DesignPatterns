c1d57315e08f438e662db482b8486f1450927225
davidkethel <davidkethel@gmail.com>
2013-09-05 11:32:58 +1000

    Started Memento pattern

---
+++ Behavioral/Memento/Person.cs
@@ -0,0 +1,26 @@
+﻿namespace DesignPatterns.Behavioral.Memento
+{
+    internal class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Height { get; set; }
+
+        public Memento CreateMemento()
+        {
+            return new Memento
+            {
+                FirstName = FirstName,
+                LastName = LastName,
+                Height = Height
+            };
+        }
+
+        public void RestoreFromMemento(Memento memento)
+        {
+            FirstName = memento.FirstName;
+            LastName = memento.LastName;
+            Height = memento.Height;
+        }
+    }
+}
