da4010d16cd8afe3a5829f890b388ac5f41f0eac
Davidkethel <davidkethel@gmail.com>
2024-05-25 18:25:35 +1000

    Added clone pattern

---
+++ Creational/Clone/Circle.cs
@@ -0,0 +1,21 @@
+﻿namespace DesignPatterns.Creational.Clone
+{
+    public class Circle : Shape
+    {
+
+        public int Radius { get; set; }
+
+        public Circle(Circle source) : base(source)
+        {
+            Radius = source.Radius;
+        }
+
+        public Circle() { }
+
+
+        public override Shape Clone()
+        {
+           return new Circle(this);
+        }
+    }
+}
---
+++ Creational/Clone/CloneExample.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DesignPatterns.Creational.Clone
+{
+    public static class CloneExample
+    {
+
+        public static List<Shape> shapes = [];
+
+        public static void Run()
+        {
+            var circle = new Circle
+            {
+                X = 4,
+                Y = 5,
+                Radius = 11
+            };
+            shapes.Add(circle);
+
+            var secondCircle = circle.Clone();
+            shapes.Add(secondCircle);
+
+            var rectangle = new Rectangle
+            {
+                X = 10,
+                Y = 10,
+                Width = 32,
+                Height = 2
+            };
+            shapes.Add(rectangle);
+
+            var shapesCopy = new List<Shape>();
+            shapesCopy.AddRange(shapes.Select(x => x.Clone()));
+        }
+    }
+}
